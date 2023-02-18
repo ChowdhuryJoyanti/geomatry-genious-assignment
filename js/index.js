931fde00f2975fbb927e5925996d95ce52287d8a
Joyantichy <120155482+Joyantichy@users.noreply.github.com>
2023-02-18 15:34:17 +0600

    function added

--- js/index.js
+++ js/index.js
@@ -3,7 +3,18 @@ document.getElementById('tringle-calculation').addEventListener('click' ,functio
    const height = document.getElementById('height-input').value;
    const a = 0.5;
    const area = parseFloat(a) * parseFloat(base)  * parseFloat( height);
-console.log( area)
+
+
+//    if(base == 'string' || height == 'string'){
+//     return alert('please give a number')
+//    }
+//    else if(base = 'negative number' || height == 'negative number'){
+//     return alert('please give me a positive number');
+//    }
+//    else{
+            
+//    }
+// console.log( area)
 //    return area.toFixed;
 //    const result = parseFloat(area).toFixed(2)
   
@@ -19,4 +30,41 @@ document.getElementById('rectangle-calculation').addEventListener('click',functi
     const length = document.getElementById('length-input').value;
     const area = parseFloat(width) * parseFloat(length);
     console.log(area);
+});
+
+document.getElementById('parallelogram-calculation').addEventListener('click',function(){
+
+   const baseInput =  document.getElementById('parallal-input-base').value;
+   const heightInput =  document.getElementById('parallal-input-height').value;
+   const totalArea = parseFloat( baseInput) * parseFloat(heightInput) ;
+   
+   console.log(totalArea);
+});
+
+
+document.getElementById('rombus-calculation').addEventListener('click',function(){
+      const d1 = document.getElementById('input-length-d1').value;
+      const d2 = document.getElementById('input-width-d2').value;
+      const roumbusMeasure = 0.5;
+      const rombusTotalArea = parseFloat(roumbusMeasure) * parseFloat(d1) * parseFloat(d2);
+
+   console.log(rombusTotalArea);
+})
+
+document.getElementById('pentagon-calculation').addEventListener('click',function(){
+   const p = document.getElementById('pentagon-p').value;
+   const b = document.getElementById('pentagon-b').value;
+   const pentagonMeasure = 0.5;
+   const pentagonArea =  parseFloat(pentagonMeasure) * parseFloat(p) * parseFloat(b);
+   console.log(pentagonArea);
+});
+
+document.getElementById('ellipse-calculation').addEventListener('click',function(){
+   const a = document.getElementById('ellipse-a').value;
+   const b  = document.getElementById('ellipse-b').value;
+   const ellipseMeasure = 3.14;
+   const ellipseArea = parseFloat(ellipseMeasure) * parseFloat(a) * parseFloat(b);
+   // return ellipseArea.toFixed(2)
+   console.log(ellipseArea.toFixed(2));
+
 });
